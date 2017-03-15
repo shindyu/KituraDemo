8b935151b9413cf59cdef896685a735e385c1c60
shindyu <shindyu@shindyu-no-MacBook-Pro.local>
2017-03-15 22:44:19 +0900

    Adds Quick, Nimble

---
+++ Tests/SourcesTests/SampleTests.swift
@@ -0,0 +1,14 @@
+import Quick
+import Nimble
+
+@testable import KituraDemo
+
+class SampleTests: QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+                expect(true).to(beTrue())
+            }
+        }
+    }
+}
